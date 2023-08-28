7a2303f2ff099e6a74792f6dcb2d07dbdf6eef0c
Mary.rani <mary.rani@stackroute.in>
2023-08-28 14:40:12 +0530

    moviedb

---
+++ src/main/java/com/stackroute/movieapi/Jobsapp1Application.java
@@ -0,0 +1,13 @@
+package com.stackroute.movieapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jobsapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jobsapp1Application.class, args);
+	}
+
+}
